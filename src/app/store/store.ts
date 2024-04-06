7ffb577931a71f275b6704f84abdec4b51282065
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-04-07 02:50:04 +0300

    add authentication

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -3,6 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { cityReducer } from 'entities/City';
 import { CommentAPI } from 'entities/Comment';
 import { OfferAPI, offersReducer } from 'entities/Offer';
+import { AuthAPI, authReducer } from 'features/Auth';
 import { offerSortReducer } from 'features/SortOffers';
 import { HttpService } from 'shared/services';
 
@@ -10,16 +11,19 @@ HttpService.configure();
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     city: cityReducer,
     offers: offersReducer,
     offerSort: offerSortReducer,
     [OfferAPI.reducerPath]: OfferAPI.reducer,
     [CommentAPI.reducerPath]: CommentAPI.reducer,
+    [AuthAPI.reducerPath]: AuthAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(OfferAPI.middleware)
-      .concat(CommentAPI.middleware),
+      .concat(CommentAPI.middleware)
+      .concat(AuthAPI.middleware),
 });
 
 export type TRootState = ReturnType<typeof store.getState>;
